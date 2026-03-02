64d7179fa1f384d86ac9711508c330a0d8764758
Claudia Napolitano <napolitano.claudia@icloud.com>
2026-03-02 15:25:04 +0100

    first commit

---
+++ Kram/Kram/KramApp.swift
@@ -0,0 +1,17 @@
+//
+//  KramApp.swift
+//  Kram
+//
+//  Created by Claudia Napolitano on 02/03/26.
+//
+
+import SwiftUI
+
+@main
+struct KramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Kram/KramTests/KramTests.swift
@@ -0,0 +1,17 @@
+//
+//  KramTests.swift
+//  KramTests
+//
+//  Created by Claudia Napolitano on 02/03/26.
+//
+
+import Testing
+@testable import Kram
+
+struct KramTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
