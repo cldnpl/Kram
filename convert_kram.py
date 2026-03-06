170fa935ed47c64d7360b1ee34dcee08f3309fc9
Elbek Mirzamakhmudov <elbekmirzamakhmudov@gmail.com>
2026-03-06 23:34:24 +0100

    deployment process

--- convert_kram.py
+++ convert_kram.py
@@ -4,7 +4,6 @@ import os
 from PIL import Image, ImageDraw, ImageFont
 
 # Carica il CSV
-# Carica il CSV
 df = pd.read_csv('dataset/external_df.csv')
 os.makedirs('dataset/images', exist_ok=True)
 
